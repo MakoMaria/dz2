cf22143a7373fe74c20a705ac98e41bdb1956502
MakoMaria <91057694+MakoMaria@users.noreply.github.com>
2023-01-09 21:51:52 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
-#include <iostream>
-#include <vector>
+#include "iostream"
+#include "t2.h"
+#include "t4.h"
+#include "t6.h"
+#include "t10.h"
+#include "t12.h"
 using namespace std;
-/*The thing is to achieve maximum profit.
- * From buying at very least price and selling at very higher price
- *
- * So, if buy at 7 & sell at any time in the future, we'll face loss.
- * Because buying price is way higher then selling price available we have
- * Now, I have seen a dip & I buy at 1 & sell at 5 my overall profit will be 5 - 1 = 4
- * But what if, I had buy at 1 & sell at 6 my profit will be 6 - 1 = 5. Which is greater then my overall profit. So, i will update my overall profit with new value.
- * Now we have done as further we don't have any higher point to sell. We will return our answer.
- * */
-int maxProfit(vector<int> &prices)
-{
-    int n = prices.size();
-    int answer = 0;
-    for (int i = 0; i < n; i++)
+
+
+int main() {
+
+    int n;
+    cout << "Enter the number of the task (2, 4, 6, 10 or 12: " << endl;
+    cin >> n;
+    if (n == 2)
     {
-        for (int j = i + 1; j < n; j++)
-        {
-            answer = max(answer, prices[j] - prices[i]);
-        }
+        task2();
     }
-    return answer;
-}
-int main()
-{
-    vector<int> prices = {7, 1, 5, 3, 6, 4};
-    cout << maxProfit(prices) << endl;
+    else if (n == 4)
+    {
+        task4();
+    }
+    else if (n == 6)
+    {
+        task6();
+    }
+    else if (n == 10)
+    {
+        task10();
+    }
+    else if (n == 12)
+    {
+        task12();
+    }
     return 0;
 }
+
+
+
